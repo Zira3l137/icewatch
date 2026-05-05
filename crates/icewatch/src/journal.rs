d3fd80bbb7493d7b4b194231894a9848e3258071
Zira3l137 <raulbairamov2002@gmail.com>
2026-05-06 00:11:21 +0400

    feat: add journal system implementation

---
+++ crates/icewatch/src/journal.rs
@@ -0,0 +1,52 @@
+use std::path::PathBuf;
+
+use chrono::DateTime;
+use chrono::Local;
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub(crate) enum ActionType {
+    Automatic,
+    Manual,
+}
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub(crate) enum Action {
+    Moved { source: PathBuf, destination: PathBuf },
+    Removed(PathBuf),
+}
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub(crate) struct Entry {
+    pub time: DateTime<Local>,
+    pub action: Action,
+    pub action_type: ActionType,
+}
+
+#[derive(Debug, Clone, Default, serde::Deserialize, serde::Serialize)]
+pub(crate) struct Journal {
+    pub(crate) entries: Vec<Entry>,
+}
+
+impl Journal {
+    pub(crate) fn log(&mut self, action: Action, action_type: ActionType) {
+        let entry = Entry { time: Local::now(), action, action_type };
+        self.entries.push(entry);
+    }
+
+    pub(crate) fn entries_before(&self, time: DateTime<Local>) -> Vec<&Entry> {
+        self.entries.iter().filter(|e| e.time <= time).collect()
+    }
+
+    #[expect(dead_code)]
+    pub(crate) fn entries_after(&self, time: DateTime<Local>) -> Vec<&Entry> {
+        self.entries.iter().filter(|e| e.time >= time).collect()
+    }
+
+    pub(crate) fn entries_between(
+        &self,
+        start: DateTime<Local>,
+        end: DateTime<Local>,
+    ) -> Vec<&Entry> {
+        self.entries.iter().filter(|e| e.time >= start && e.time <= end).collect()
+    }
+}
